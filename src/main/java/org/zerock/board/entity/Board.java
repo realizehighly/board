9422d39ccebeee15c034f9d2fc29a030a9ef28f4
Heonjun Jeong <gjswns1991@gmail.com>
2022-02-24 18:20:38 +0900

    Initial commit

---
+++ src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,25 @@
+package org.zerock.board.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString(exclude = "writer") //@ToString 은 항상 exclude
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    private Member writer; //연관관계 지정
+}
