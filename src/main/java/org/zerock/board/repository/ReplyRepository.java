9ab2896bd45539734fedf2b361682d0d6973ad2a
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-01 17:16:55 +0900

    서비스 계층

--- src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -1,7 +1,15 @@
 package org.zerock.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    //특정 게시물 번호(bno)로 댓글을 삭제하는 기능
+    @Modifying
+    @Query("delete from Reply r where  r.board.bno =:bno")
+    void deleteByBno(@Param("bno") Long bno);
 }
--- src/main/java/org/zerock/board/service/BoardService.java
+++ src/main/java/org/zerock/board/service/BoardService.java
@@ -1,12 +1,20 @@
 package org.zerock.board.service;
 
 import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
 public interface BoardService {
 
     Long register(BoardDTO dto);
+
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO); //목록처리
+
+    BoardDTO get(Long bno); //조회
+
+    void removeWithReplies(Long bno); //삭제
 
     default Board dtoToEntity(BoardDTO dto){
 
@@ -20,5 +28,21 @@ public interface BoardService {
                 .build();
 
         return board;
+    }
+
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount) {
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue())//long 으로 나오므로 int 로 처리
+                .build();
+
+        return boardDTO;
     }
 }
