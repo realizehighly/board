6026622cbbe4a52d569139acfae16aa7d852c7bd
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-09 22:14:25 +0900

    JSON과 Ajax로 댓글 처리

--- src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
@@ -12,4 +15,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Modifying
     @Query("delete from Reply r where  r.board.bno =:bno")
     void deleteByBno(@Param("bno") Long bno);
+
+    //게시물로 댓글 목록 가져오기
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
